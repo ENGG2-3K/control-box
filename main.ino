0f48ce8b4979de79a059128879dee33f83a5d608
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2020-09-02 21:36:13 +1000

    Fix overflow every 50 days

--- main.ino
+++ main.ino
@@ -5,6 +5,8 @@ unsigned long last_button_press_time;
 
 // Global array of buttons
 button pressed_button;
+
+// Represents the info we receive from the mega
 mega_info rcvd_info;
 
 void setup()
@@ -17,16 +19,19 @@ void setup()
 void loop()
 {
 
-    // Update the LCD with the information we received from the mega and any info we want inputted by the latest pressed button
+    // Update the LCD with the information we received from the mega and any info we want inputted
+    // by the latest pressed button
     update_lcd(pressed_button, rcvd_info);
 
-    // Check if the last pressed button was the emergency button or the mega has told us to go into the emergency state
+    // Check if the last pressed button was the emergency button or the mega has told us to go
+    // into the emergency state
     if (pressed_button.chars[0] == 'e' || rcvd_info.rcvd_char == RCV_EMERGENCY_CHAR)
     {
         enter_emergency_state();
     }
-    // If there has been a long enough timeout since the last button press, then update the last button press time, get the newly pressed button and send the relevant char to the mega
-    else if (millis() - last_button_press_time > BUTTON_TIMEOUT)
+    // If there has been a long enough timeout since the last button press, then update the last
+    // button press time, get the newly pressed button and send the relevant char to the mega
+    else if (abs(millis() - last_button_press_time > BUTTON_TIMEOUT))
     {
         pressed_button = get_pressed_button(buttons);
         char index_of_char = pressed_button.chars[pressed_button.toggled];
@@ -34,6 +39,7 @@ void loop()
         last_button_press_time = millis();
     }
 
-    // Check if there is information from the mega that has been sent to us and store it into a mega_info struct
+    // Check if there is information from the mega that has been sent to us and store it into a
+    // mega_info struct
     rcvd_info = check_link_buffer();
 }
