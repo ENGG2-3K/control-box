fc886a39cd03de8053a505b39c8d724247fb464a
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2020-10-27 15:38:45 +1100

    Change button timeout to 500ms

--- include/lib.h
+++ include/lib.h
@@ -10,7 +10,7 @@ const char BUTTON_DEBUG_CHAR = 'b';
 
 const int NUM_BUTTONS = 4;
 const char BUTTON_START_PIN = 2;
-const int BUTTON_TIMEOUT = 1000; // 1 second
+const int BUTTON_TIMEOUT = 500; // 0.5 second
 const char EMERGENCY_STOP_CHAR = 'x';
 const char EAST_CHAR = 'e';
 const char WEST_CHAR = 'w';
@@ -41,7 +41,7 @@ button debug_get_pressed_button(button *buttons, char *debug_buffer);
 
 bool debug_link_info_available(char* debug_buffer);
 
-mega_info debug_check_link_buffer(char *debug_buffer);
+mega_info debug_check_link_buffer(char debug_buffer[]);
 mega_info check_link_buffer(SoftwareSerial &bt);
 
 void debug_print_rcvd_info(mega_info rcvd_info);
